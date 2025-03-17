efc2a797d95385408cf58a9833cd0db69a9e5b4b
ulrich.barnstedt@gmail.com <ulrich.barnstedt@gmail.com>
2025-03-17 10:00:30 +0100

    Refactor client/index.ts, add click events for nodes

---
+++ src/client/graphData.ts
@@ -0,0 +1,65 @@
+import {Graph} from "../shared/graph/Graph";
+import {deserialize} from "@ungap/structured-clone";
+import {ColorMap, paletteDark, paletteLight} from "./colors";
+
+export const getCurrentGraph = async () : Promise<Graph> => {
+    const apiResponse = await fetch("/graph");
+    const json = await apiResponse.json();
+
+    return Graph.fromSerialized(deserialize(json));
+}
+
+export const preprocessData = (graph: Graph) : {data: any}[] => {
+    const elements = [];
+    const kindColorMap = new ColorMap(paletteLight);
+    const namespaceColorMap = new ColorMap(paletteDark);
+    namespaceColorMap.defineFixedColor("", "#ffffff");
+
+    const emphasizedTypes = {
+        "V1Namespace" : true,
+        "V1Node" : true
+    }
+    const getNodeStyling = (type: string): Record<string, any> => {
+        if (type in emphasizedTypes) {
+            return {
+                height: 50,
+                width: 50
+            };
+        } else {
+            return {
+                height: 30,
+                width: 30
+            };
+        }
+    }
+
+    for (const node of graph.getAllNodes()) {
+        elements.push({
+            data: {
+                name: node.kubeObj.metadata?.name!,
+                nodeColor: kindColorMap.getColor(node.kind),
+                ...node,
+                ...getNodeStyling(node.kind)
+            }
+        })
+    }
+    for (const relation of graph.getAllRelations()) {
+        let isSameNamespace =
+            relation.from.kubeObj.metadata?.namespace! === relation.to.kubeObj.metadata?.namespace! ||
+            relation.to.kind === "V1Namespace" ||
+            relation.from.kind === "V1Namespace";
+        let namespace = relation.from.kubeObj.metadata?.namespace! || relation.to.kubeObj.metadata?.namespace!;
+
+        elements.push({
+            data: {
+                id: relation.from.id + "+" + relation.to.id,
+                source: relation.from.id,
+                target: relation.to.id,
+                linkColor: isSameNamespace ? namespaceColorMap.getColor(namespace) : "white"
+            }
+        })
+    }
+
+    return elements;
+}
+
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,69 +1,6 @@
-import {deserialize} from "@ungap/structured-clone";
-import {Graph} from "../shared/graph/Graph";
 import cytoscape from "cytoscape";
 import euler from 'cytoscape-euler';
-import {ColorMap, paletteDark, paletteLight} from "./colors";
-
-const getCurrentGraph = async () : Promise<Graph> => {
-    const apiResponse = await fetch("/graph");
-    const json = await apiResponse.json();
-
-    return Graph.fromSerialized(deserialize(json));
-}
-
-const preprocessData = (graph: Graph) : {data: any}[] => {
-    const elements = [];
-    const kindColorMap = new ColorMap(paletteLight);
-    const namespaceColorMap = new ColorMap(paletteDark);
-    namespaceColorMap.defineFixedColor("", "#ffffff");
-
-    const emphasizedTypes = {
-        "V1Namespace" : true,
-        "V1Node" : true
-    }
-    const getNodeStyling = (type: string): Record<string, any> => {
-        if (type in emphasizedTypes) {
-            return {
-                height: 50,
-                width: 50
-            };
-        } else {
-            return {
-                height: 30,
-                width: 30
-            };
-        }
-    }
-
-    for (const node of graph.getAllNodes()) {
-        elements.push({
-            data: {
-                name: node.kubeObj.metadata?.name!,
-                nodeColor: kindColorMap.getColor(node.kind),
-                ...node,
-                ...getNodeStyling(node.kind)
-            }
-        })
-    }
-    for (const relation of graph.getAllRelations()) {
-        let isSameNamespace =
-            relation.from.kubeObj.metadata?.namespace! === relation.to.kubeObj.metadata?.namespace! ||
-            relation.to.kind === "V1Namespace" ||
-            relation.from.kind === "V1Namespace";
-        let namespace = relation.from.kubeObj.metadata?.namespace! || relation.to.kubeObj.metadata?.namespace!;
-
-        elements.push({
-            data: {
-                id: relation.from.id + "+" + relation.to.id,
-                source: relation.from.id,
-                target: relation.to.id,
-                linkColor: isSameNamespace ? namespaceColorMap.getColor(namespace) : "white"
-            }
-        })
-    }
-
-    return elements;
-}
+import {getCurrentGraph, preprocessData} from "./graphData";
 
 (async () => {
     const apiGraph = await getCurrentGraph();
@@ -111,7 +48,7 @@ const preprocessData = (graph: Graph) : {data: any}[] => {
                     ((connections === 0 ? 1 : connections) * 5) +
                     (data.name.length * 0.5);
 
-                // WARNING: returning 0 will cause INFNITE ram usage
+                // WARNING: returning 0 will cause INFINITE ram usage
                 return mass > 0 ? mass : 1;
             }
         },
@@ -124,12 +61,17 @@ const preprocessData = (graph: Graph) : {data: any}[] => {
         }
     });
 
-    cy.on('tap', 'node', function(evt) {
-        const node = evt.target;
-        const data = node.data();
+    cy.on("tap", (ev) => {
+        // WebGL does not properly output nodes, therefore slowly find the correct node
+        const node = Array.from(cy.nodes()).find(node => {
+            const bb = node.boundingbox({includeLabels: false});
 
-        console.log(node.width(), node.height());
+            return bb.x1 <= ev.position.x &&
+                bb.x2 >= ev.position.x &&
+                bb.y1 <= ev.position.y &&
+                bb.y2 >= ev.position.y;
+        });
 
-        // TODO
-    });
+        console.log(node);
+    })
 })();
